5466bd1b4f24538d4555bb845271eb8f37ef23c8
JUNIOR ALVES <jjalves96@gmail.com>
2021-07-27 19:18:56 -0300

    styles: config styled-components and global theme

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,34 @@
+export default {
+  colors: {
+    primary: '#5636d3',
+    secondary: '#ff872c',
+    secondary_light: 'rgba(255, 135, 44, 0.3)',
+
+    sucess: '#12a454',
+    sucess_light: 'rgba(18, 168, 84, 0.5)',
+
+    attention: '#e83f5b',
+    attention_light: 'rgba(232, 63, 91, 0.5)',
+
+    shape: '#fff',
+    title: '#363f5f',
+    text: '#969cb2',
+    text_dark: '#000',
+    background: '#f0f2f5'
+  },
+  fonts: {
+    regular: 'Poppins_400Regular',
+    medium: 'Poppins_500Medium',
+    bold: 'Poppins_700Bold',
+  },
+  spacings: {
+    xxxsmall: '0.8rem',
+    xxsmall: '1rem',
+    xsmall: '1.6rem',
+    small: '2.4rem',
+    medium: '3.2rem',
+    large: '4.0rem',
+    xlarge: '4.8rem',
+    xxlarge: '5.6rem'
+  }
+} as const;
